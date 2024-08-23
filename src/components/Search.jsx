9f11926d30f53b3e75298b2b8fea3e708d54f423
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-08-23 21:51:59 +0300

    add suggestion section

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,13 +3,20 @@ import './Search.css'
 
 function Search() {
   return (
-    <div className="search__container pt-5">
-      <div className="search">
-        <span className="searchType">Search in Names</span>
-        <input type="text" />
-        <CiSearch size={24} />
+      <div className="search__container pt-5">
+        <div className="search">
+          <span className="searchType">Search in Names</span>
+          <input type="text" />
+          <CiSearch size={24} />
+        </div>
+        <div className="card suggest">
+          <ul className="list-group">
+            <li className="list-group-item">An item</li>
+            <li className="list-group-item">A second item</li>
+            <li className="list-group-item">A third item</li>
+          </ul>
+        </div>
       </div>
-    </div>
   );
 }
 
