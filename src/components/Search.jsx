94dfc1c1de61ce5c1ff7e04c2c46abc918d8769f
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-08-23 16:13:33 +0300

    add search icon

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,15 +1,16 @@
-
+import { CiSearch } from "react-icons/ci";
 
 function Search() {
   return (
-      <div className="container">
-          <div className="search mt-5">
-              <span className="searchType">
-                 <input type="text" /> 
-              </span>
-          </div>
+    <div className="container">
+      <div className="search mt-5">
+        <span className="searchType">
+          <input type="text" />
+        </span>
+        <CiSearch />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Search
+export default Search;
