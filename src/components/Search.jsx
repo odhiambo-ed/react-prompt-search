b1ec74b70b4c443905a13aca2001a42014017d59
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-08-23 21:52:31 +0300

    edit suggest

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -12,8 +12,6 @@ function Search() {
         <div className="card suggest">
           <ul className="list-group">
             <li className="list-group-item">An item</li>
-            <li className="list-group-item">A second item</li>
-            <li className="list-group-item">A third item</li>
           </ul>
         </div>
       </div>
